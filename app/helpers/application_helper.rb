98bca7b80f012a987ec8b1eb1aa62aad02b8c608
OhtakeYusuke <o.yusuke0501@gmail.com>
2020-12-30 22:55:03 +0900

    meta_tag成功

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  def default_meta_tags
+    {
+      site: "pra16",
+      title: "p16",
+      separator: "|",
+      icon: {href: image_url('default.png')},
+      og: {
+        site_name: "pra16",
+        title: "p16",
+        description: "ディスクリプション",
+        type: "website"
+      }
+    }
+  end
 end
